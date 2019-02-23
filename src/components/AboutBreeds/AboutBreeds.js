ad1589d3f1902d40875189bffb5c040a91d50c34
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-23 09:59:42 -0700

    Add basic images to AboutBreeds

--- src/components/AboutBreeds/AboutBreeds.js
+++ src/components/AboutBreeds/AboutBreeds.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { breeds } from '../../staticData/breeds'
+import DogCard from '../DogCard/DogCard'
 
-export const AboutBreeds = () => {
-  return (
-    <div>About Breeds</div>
-  )
+export class AboutBreeds extends Component {
+  constructor() {
+    super()
+    this.state = {
+      breeds: breeds
+    }
+  }
+
+  render() {
+// debugger
+    const breedCards =this.state.breeds.map((breed, i) => {
+      return <DogCard {...breed} cardNumber={i} />
+    })
+
+
+    return (
+      <div> {breedCards}</div >
+    )
+  }
 }
 
 export default AboutBreeds
--- src/thunks/fetchDogs.js
+++ src/thunks/fetchDogs.js
@@ -4,6 +4,7 @@ import {fetchDogsSuccess} from '../actions'
 import {dogCleaner} from '../helpers/dogCleaner'
 
 export const fetchDogs = (options) => {
+  console.log(options)
   return async (dispatch) => {
     try {
       // dispatch(isLoading(true))
@@ -12,13 +13,16 @@ export const fetchDogs = (options) => {
       })
       if (!response.ok) {
         throw Error(response.statusText)
+       
       }
       const dogs = await response.json()
       // dispatch(isLoading(false))
       // const cleanedDogs = dogCleaner(dogs)
       // debugger
+      const cleanedDogs = dogCleaner(dogs)
       const lastOffset = dogs.petfinder.lastOffset.$t
-      dispatch(fetchDogsSuccess(options.location, options.breed, lastOffset, dogCleaner(dogs)))
+
+      dispatch(fetchDogsSuccess(options.location, options.breed, lastOffset, cleanedDogs))
     } catch (error) {
       // dispatch(hasErrored(error.message))
     }
