53c010dda537e80399a28dc3accd2e4634dfd435
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-23 09:58:19 -0700

    Add static breed info

---
+++ src/staticData/breeds.js
@@ -0,0 +1,26 @@
+const breeds = [
+  { breed: 'Siberian Husky', img: '../../images/siberian-husky.jpg', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Australian Shepherd', img: '../../images', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'German Shorthaired Pointer', img: '../../images', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Vizsla', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Australian Cattle Dog', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Weimaraner', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Bernese Mountain Dog', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Rhodesian Ridgeback', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Alaskan Malamute', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Portugese Water Dog', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Border Collie', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'German Shepherd Dog', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Dalmation', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Airedale Terrier', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Jack Russell Terrier', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Poodle', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Belgian Shepherd', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'English Setter', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Brittany Spaniel', img: '', isCold: false, isRunner: true, isClimber: true },
+  { breed: 'Labrador Retriever', img: '', isCold: false, isRunner: true, isClimber: true }
+]
+
+module.exports = {
+  breeds
+}
