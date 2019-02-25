1e39fc595871072564a1c59dfa043cdfd872892d
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-25 14:44:09 -0700

    Add addDistance thunk

---
+++ src/thunks/addDistance.js
@@ -0,0 +1,28 @@
+// import { isLoading, hasErrored, fetchDogsSuccess} from '../actions'
+
+import { googleDistanceMatrixAPI } from '../APIKEY'
+
+export const addDistance = (dogs, zipCode) => {
+
+  return (dispatch) => {
+
+    const promisedDogs = dogs.map(async dog => {
+
+      try {
+        const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${zipCode}&destinations=${dog.zip}&key=${googleDistanceMatrixAPI}`
+        const response = await fetch(url)
+        if (!response.ok) {
+          debugger
+          throw Error(response.statusText)
+        }
+        const data = await response.json()
+        let newDog = { ...dog }
+        newDog.distance = data.rows[0].elements[0].distance.text
+        return newDog
+      } catch (error) {
+        // dispatch(hasErrored(error.message))
+      }
+    })
+    return Promise.all(promisedDogs)
+  }
+}
