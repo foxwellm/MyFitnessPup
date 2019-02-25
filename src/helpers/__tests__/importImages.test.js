f1223f966d44584648d86009fc7ff46686053188
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-25 08:53:34 -0700

    Add importImages tests

---
+++ src/helpers/__tests__/importImages.test.js
@@ -0,0 +1,14 @@
+import {importImages} from '../importImages'
+import * as image from '../importImages'
+
+
+describe('importImages', () => {
+  // it('should', () => {
+  //   const mockKeys = {
+  //     0: "../images./airedale-terrier.jpg",
+  //     1: "../images./alaskan-malamute.jpg"
+  //   }
+  //   image.importImages = jest.fn().mockImplementation(() => mockKeys)
+  //   expect(importImages()).toEqual('../')
+  // })
+})
