d7f9c062c40db107fb05807eaf2f6b82583d2014
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-25 08:54:42 -0700

    Add BreedCard Tests

--- src/components/BreedCard/BreedCard.js
+++ src/components/BreedCard/BreedCard.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { importImages } from '../../helpers/importImages'
 
 
 export class BreedCard extends Component {
   constructor() {
     super()
   }
-  importImages = (req) => {
-    let images = {};
-    req.keys().map((item) => { images[item.replace('./', '')] = req(item); });
-    return images;
-  }
+
+
+
 
   render() {
-    const images = this.importImages(require.context('../../images'));
+    const images = importImages();
     return (
-      
+
       <Link to={`/about-breeds/${this.props.breed}`}>
         <div className='BreedCard-container' onClick={this.handleClick}>
           <div className='breed-card-header'>{this.props.breed}</div>
           <div className='breed-img' style={{ backgroundImage: `url(${images[this.props.img]})` }}>
+            <div className='breed-attr'></div>
           </div>
         </div>
       </Link>
+
     )
   }
 
--- src/components/BreedCard/BreedCard.test.js
+++ src/components/BreedCard/BreedCard.test.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import BreedCard from './BreedCard'
+import * as image from '../../helpers/importImages'
 
 describe('BreedCard', () => {
   let wrapper
-  const importImages = jest.fn()
-  // const require = jest.fn()
-  const require = {
-    context: jest.fn()
-  }
+  image.importImages = jest.fn().mockImplementation(() => '../')
+
   it('should have match the correct snapshot', () => {
     wrapper = shallow(<BreedCard />)
     expect(wrapper).toMatchSnapshot()
   })
+
 })
