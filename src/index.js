89c479949546306b4d0056c1cf0112da76b0f965
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-24 17:23:57 -0700

    Move rootReducer to index file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import { rootReducer } from './reducers/rootReducer'
+import { rootReducer } from './reducers'
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
