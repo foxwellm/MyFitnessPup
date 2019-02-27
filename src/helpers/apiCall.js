c6162f201327ea37f4e4dba3483a2d7a2a30e6ff
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-27 05:33:26 -0700

    Remove old fetch calls

--- src/helpers/apiCall.js
+++
@@ -1,8 +0,0 @@
-export const apiCall = async (url, options) => {
-  const response = await fetch(url, options)
-  if (response.ok) {
-    return await response.json()
-  } else {
-    throw new Error("Error")
-  }
-}
--- src/helpers/fetchDogLocation.js
+++
@@ -1,9 +0,0 @@
-export const fetchDogLocation = async (startLocation, endLocation) => {
-  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${startLocation}&destinations=${endLocation}&key=AIzaSyD7FdmtnkrRQem_7NJWaKpHdNArXXgm7k4`);
-  if (!response.ok) {
-    debugger
-    throw Error(response.statusText)
-  }
-  const result = await response.json()
-  return result.rows[0].elements[0].distance.text
-}
