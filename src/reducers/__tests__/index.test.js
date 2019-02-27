f9f57c6c15696cfef9809d1cebf43ea5b8162a52
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-27 07:52:28 -0700

    Add reducer index tests

---
+++ src/reducers/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import { createStore } from 'redux';
+import { rootReducer } from '../index';
+
+describe('rootReducer', () => {
+  let store = createStore(rootReducer)
+
+  it('should set the store with an initial state', () => {
+    let expected = {
+      storedDogs: {},
+      isDisplay: false,
+      isLoading: true,
+    }
+
+    expect(store.getState()).toEqual(expected)
+  })
+})
