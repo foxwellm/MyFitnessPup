2345975ad5d12b33cb74eb192c9a19aad6bca317
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-27 05:34:29 -0700

    Style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import dog from '../../images/dog.png'
 
 export const Header = () => {
   return (
     <div className='Header'>
-      <NavLink to='/' className='header-navlink'>MyFitnessPup</NavLink>
+      <NavLink to='/' className='title-navlink'>MyFitnessPup</NavLink> 
       <NavLink to='/about-breeds' className='header-navlink' activeClassName='selected'>About Breeds</NavLink>
       <NavLink to='/search' className='header-navlink' activeClassName='selected'>Search</NavLink>
+      {/* <img className='header-img' src={dog}></img> */}
     </div>
   )
 }
