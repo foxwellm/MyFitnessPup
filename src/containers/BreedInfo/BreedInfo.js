fd536b24f11ea71fd1e1d57015b3b3ccfff22052
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-27 05:39:08 -0700

    Add BreedInfo styling

--- src/containers/BreedInfo/BreedInfo.js
+++ src/containers/BreedInfo/BreedInfo.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react'
 import AboutBreeds from '../../components/AboutBreeds/AboutBreeds';
+import {Link} from 'react-router-dom'
+import {importImages} from '../../helpers/importImages'
 
 export class BreedInfo extends Component {
+
   render() {
+    const {img, isCold, isRunner, isClimber} = this.props.breed
+    const images = importImages();
     return (
       <div>
         <AboutBreeds />
         <div className='breed-info-container'>
-        <div className='form'>
-        
+        <div className='breed-info-card'>
+            <div className='back-btn-container'><Link to='/about-breeds'><button className='breed-info-btn'>Back</button></Link></div>
+            <p>{this.props.info.desc}</p>
+            <div className='breed-info-img' style={{ backgroundImage: `url(${images[img]})` }}></div>
         </div>
         </div>
 
