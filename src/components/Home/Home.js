823531c044111fea51d73bb33fcdc93310c5b2af
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-23 13:08:48 -0600

    Add background image to Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <div className='home-container'>
+    <img className='home-img' src={require("../../assets/mountains.jpg")}/>
       <div className='home-card'>MyFitnessPup is here to help you find the perfect dog to take hiking/running with you. Learn about the breeds best suited for high activity, then find dogs that are in shelters right by you to take on your next adventure.</div>
     </div>
   )
