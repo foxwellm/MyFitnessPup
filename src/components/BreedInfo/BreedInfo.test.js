b38c627233c426ff61a2c29883f22c5f1216995f
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-26 09:30:12 -0600

    Update BreedInfo tests

--- src/components/BreedInfo/BreedInfo.test.js
+++ src/components/BreedInfo/BreedInfo.test.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import BreedInfo from './BreedInfo'
-import * as image from '../../helpers/importImages'
 
 describe('BreedInfo', () => {
   let wrapper
-  image.importImages = jest.fn().mockImplementation(() => '../')
+  let mockBreed
+  let mockInfo
   it('should have match the correct snapshot', () => {
-    const breed = {
-      img: 'Poodle'
+    mockBreed = {
+      breed: 'Siberian Husky',
+      tag: 'siberian-husky',
+      img: 'siberian-husky.jpg',
+      isCold: true,
+      isRunner: true,
+      isClimber: true
     }
-    const info = {
-      desc: 'Happy Doggy'
-      }
-    wrapper = shallow(<BreedInfo breed={breed} info ={info} />)
+    mockInfo = {
+      name: 'Siberian Husky', desc: "The Siberian Husky was originally bred to pull sleds and carts long distances in the harsh Russian climate.They are powerfully built dogs with lively spirits, always ready for adventure at any time.A true endurance breed, Siberians make an excellent choice for those looking for a dog that will keep them entertained on long hikes.They are gentle and alert, but very social dogs that enjoy spending time outside."
+    }
+
+    wrapper = shallow(<BreedInfo breed={mockBreed} info={mockInfo} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
