9a089b9c50347851d70cf34266b747edcf393ff4
Matthew Foxwell <foxwellmw@gmail.com>
2019-04-26 09:08:23 -0600

    Update AboutBreeds and BreedCard tests

--- src/components/AboutBreeds/AboutBreeds.test.js
+++ src/components/AboutBreeds/AboutBreeds.test.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import AboutBreeds from './AboutBreeds'
 
+
 describe('AboutBreeds', () => {
   let wrapper
   it('should have match the correct snapshot', () => {
-    let mockStaticBreeds = [{ breed: 'Husky' }, { breed: 'Chihuahua' }]
-    let mockStaticBreedInfo = [{ name: 'Husky' }, { name: 'Chihuahua' }]
-    let mockLocation = '77043'
+    let mockStaticBreeds = [
+      { breed: 'Siberian Husky', tag: 'siberian-husky', img: 'siberian-husky.jpg', isCold: true, isRunner: true, isClimber: true },
+      { breed: 'Australian Shepherd', tag: 'australian-shepherd', img: 'australian-shepherd.jpg', isCold: true, isRunner: false, isClimber: true },
+    ]
+    let mockStaticBreedInfo = [
+      { name: 'Siberian Husky', desc: "The Siberian Husky was originally bred to pull sleds and carts long distances in the harsh Russian climate.They are powerfully built dogs with lively spirits, always ready for adventure at any time.A true endurance breed, Siberians make an excellent choice for those looking for a dog that will keep them entertained on long hikes.They are gentle and alert, but very social dogs that enjoy spending time outside." },
+      { name: 'Australian Shepherd', desc: "The Australian Shepherd is an intelligent, adventurous breed that is always ready to go. Originally used as herding dogs, they still excel at this job today, as well as in many dog sports such as agility and obedience. Australian Shepherds make excellent hiking partners, as they are very athletic, friendly, and well suited for long hours of strenuous activity." },
+    ]
+    let mockLocation = {pathname: '/about-breeds'}
     wrapper = shallow(<AboutBreeds
       staticBreeds={mockStaticBreeds}
       staticBreedInfo={mockStaticBreedInfo}
--- src/components/BreedCard/BreedCard.test.js
+++ src/components/BreedCard/BreedCard.test.js
@@ -5,13 +5,32 @@ import BreedCard from './BreedCard'
 describe('BreedCard', () => {
   let wrapper
   let mockImg
+  let mockLocation
+  let mockHandleSearchFilter
+  let mockBreed
+  let mockActive
 
   beforeEach(() => {
     mockImg = 'siberian-husky.jpg'
+    mockLocation = { pathname: '/about-breeds' }
+    mockHandleSearchFilter = jest.fn()
+    mockBreed = {
+      breed: 'Siberian Husky',
+      tag: 'siberian-husky',
+      img: 'siberian-husky.jpg',
+      isCold: true,
+      isRunner: true,
+      isClimber: true
+    }
+    mockActive = true
 
     wrapper = shallow(
       <BreedCard
         img={mockImg}
+        location={mockLocation}
+        handleSearchFilter={mockHandleSearchFilter}
+        breed={mockBreed}
+        active={mockActive}
       />
     )
   });
@@ -20,4 +39,37 @@ describe('BreedCard', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should match the correct snapshot when the path is not "/about-breeds" ', () => {
+    mockLocation = { pathname: '/about' }
+    wrapper = shallow(
+      <BreedCard
+        img={mockImg}
+        location={mockLocation}
+        handleSearchFilter={mockHandleSearchFilter}
+        breed={mockBreed}
+      />
+    )
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('handleClick', () => {
+    it('should not call handleSearchFilter if location.pathname is "/about-breeds" ', () => {
+      wrapper.instance().handleClick()
+      expect(mockHandleSearchFilter).not.toHaveBeenCalledWith(mockBreed)
+    })
+
+    it('should call handleSearchFilter with correct params if ocation.pathname is not "/about-breeds"', () => {
+      mockLocation = { pathname: '/about' }
+      wrapper = shallow(
+        <BreedCard
+          img={mockImg}
+          location={mockLocation}
+          handleSearchFilter={mockHandleSearchFilter}
+          breed={mockBreed}
+        />
+      )
+      wrapper.instance().handleClick()
+      expect(mockHandleSearchFilter).toHaveBeenCalledWith(mockBreed)
+    })
+  })
 })
