e0950222794be327ff6bb51e513f3da47952b474
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-14 20:35:49 -0600

    Update retrieveDogs thunk and tests

--- src/thunks/__tests__/retrieveDogs.tests.js
+++ src/thunks/__tests__/retrieveDogs.tests.js
@@ -1,28 +1,28 @@
 import { retrieveDogs } from '../retrieveDogs'
-import { hasErrored, setLoading, fetchDogsSuccess } from '../../actions'
+import { hasErrored, setLoading, fetchDogsSuccess, setDogsNext, fetchAdditionalDogsSuccess } from '../../actions'
 import * as cleaner from '../../helpers/dogCleaner'
 
 describe('retrieveDogs', () => {
-
+  
   let mockDogs
   let mockZipCode
   let mockDispatch
-  let mockURL
-
+  let mockNextSearch
+  
   beforeEach(() => {
-    mockURL = 'http://localhost:3000/'
     mockDispatch = jest.fn()
     mockDogs = ['Poodle']
     mockZipCode = 77043
+    mockNextSearch = 'nextUrl'
   })
-
-
-  it('calls dispatch with the isLoading action', () => {
-    const thunk = retrieveDogs(mockZipCode, mockDogs)
+  
+  
+  it('calls dispatch with the setLoading action', () => {
+    const thunk = retrieveDogs(mockZipCode, mockDogs )
     thunk(mockDispatch)
     expect(mockDispatch).toHaveBeenCalledWith(setLoading(true))
   })
-
+  
   it('should dispatch hasErrored with a message if the response is not "ok"', async () => {
     window.fetch = jest.fn().mockImplementation(() =>
       Promise.resolve({
@@ -33,45 +33,109 @@ describe('retrieveDogs', () => {
     await thunk(mockDispatch)
     expect(mockDispatch).toHaveBeenCalledWith(hasErrored('Something went wrong'))
   })
-
+  
   it('should dispatch setLoading(false) if the response is ok', async () => {
-    Promise.all = jest.fn().mockImplementation(() => [])
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+        pagination: {
+          total_pages: 5,
+          _links: {
+            next: {
+              href: 'previousLink'
+            }
+          }
+        }
+      })
+      }))
+    cleaner.dogCleaner = jest.fn().mockImplementation(() => ['Poodle'])
     const thunk = retrieveDogs(mockZipCode, mockDogs)
     await thunk(mockDispatch)
     expect(mockDispatch).toHaveBeenCalledWith(setLoading(false))
   })
-
-  it('should dispatch fetchDogsSuccess if resolvedDogs is returned', async () => {
-    Promise.all = jest.fn().mockImplementation(() => [])
-    const thunk = retrieveDogs(mockZipCode, mockDogs)
-    await thunk(mockDispatch)
-    expect(mockDispatch).toHaveBeenCalledWith(fetchDogsSuccess([]))
-  })
-
-  it('should dispatch fetchDogsSuccess when mapping through dogs', async () => {
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      ok: true,
-      json: () => Promise.resolve({
-        petfinder: {
-          lastOffset: {
-            $t: 'pictureURL'
-          }
-        },
-        data: {
-          rows: [{
-            elements: [{
-              distance: {
-                text: '12'
+  
+  it('should dispatch setDogsNext() with the next link if there is one', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+          pagination: {
+            total_pages: 5,
+            _links: {
+              next: {
+                href: 'nextLink'
               }
-            }]
-          }]
-        }
-      })
-    }))
+            }
+          }
+        })
+      }))
     cleaner.dogCleaner = jest.fn().mockImplementation(() => ['Poodle'])
     const thunk = retrieveDogs(mockZipCode, mockDogs)
     await thunk(mockDispatch)
-    await expect(mockDispatch).toHaveBeenCalledWith(fetchDogsSuccess([]))
+    expect(mockDispatch).toHaveBeenCalledWith(setDogsNext('nextLink'))
+  })
+  
+  it('should not dispatch setDogsNext() if there is no next link', async () => {
+    const setDogsNext = jest.fn()
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+          pagination: {
+            total_pages: 5,
+            _links: {
+              prev: {
+                href: 'nextLink'
+              }
+            }
+          }
+        })
+      }))
+    cleaner.dogCleaner = jest.fn().mockImplementation(() => ['Poodle'])
+    const thunk = retrieveDogs(mockZipCode, mockDogs)
+    await thunk(mockDispatch)
+    expect(setDogsNext).not.toHaveBeenCalled()
   })
 
+  it('should dispatch fetchDogsSuccess if retrieveDogs thunk does not receive a nextSearch argument', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+          pagination: {
+            total_pages: 5,
+            _links: {
+              next: {
+                href: 'previousLink'
+              }
+            }
+          }
+        })
+      }))
+    const thunk = retrieveDogs(mockZipCode, mockDogs)
+    await thunk(mockDispatch)
+    expect(mockDispatch).toHaveBeenCalledWith(fetchDogsSuccess())
+  })
+  
+  it('should dispatch fetchAdditionalDogsSuccess if retrieveDogs thunk receives a nextSearch argument', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({
+          pagination: {
+            total_pages: 5,
+            _links: {
+              next: {
+                href: 'previousLink'
+              }
+            }
+          }
+        })
+      }))
+    const thunk = retrieveDogs(mockZipCode, mockDogs, mockNextSearch)
+    await thunk(mockDispatch)
+    expect(mockDispatch).toHaveBeenCalledWith(fetchAdditionalDogsSuccess())
+  })
+  
 })
--- src/thunks/retrieveDogs.js
+++ src/thunks/retrieveDogs.js
@@ -1,27 +1,33 @@
 import { dogCleaner } from '../helpers/dogCleaner'
 import { addDistance } from '../thunks/addDistance'
 import { getPetFinderToken } from '../thunks/getPetFinderToken'
-import { fetchDogsSuccess, setDogsNext, setLoading, hasErrored } from '../actions'
+import { fetchDogsSuccess, fetchAdditionalDogsSuccess, setDogsNext, setLoading, hasErrored, setSearchTotalPages } from '../actions'
 
-export const retrieveDogs = (zipCode, dogs) => {
+export const retrieveDogs = (zipCode, dogs, nextSearch) => {
   return async (dispatch) => {
     try {
       dispatch(setLoading(true))
       const petFinderAccessToken = await dispatch(getPetFinderToken())
       const dogsToSearch = dogs.join(',')
-      const petFinderRequestUrl = `https://api.petfinder.com/v2/animals?type=dog&breed=${dogsToSearch}&location=${zipCode}&age=baby,young,adult`
+      const petFinderUrl = 'https://api.petfinder.com'
+      const petFinderSearchParams = nextSearch || `/v2/animals?type=dog&breed=${dogsToSearch}&location=${zipCode}&age=baby,young,adult&sort=distance&limit=10&distance=10`
       const petFinderRequestUrlOptions = {
         headers: {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${petFinderAccessToken}`
         }
       }
-      const petFinderResponse = await fetch(petFinderRequestUrl, petFinderRequestUrlOptions)
+      const petFinderResponse = await fetch(petFinderUrl + petFinderSearchParams, petFinderRequestUrlOptions)
+      if (!petFinderResponse.ok) {
+        throw Error(petFinderResponse.statusText)
+      }
       const dirtyDogs = await petFinderResponse.json()
       const cleanedDogs = dogCleaner(dirtyDogs)
       const cleanedAndAddedDogs = await dispatch(addDistance(cleanedDogs, zipCode))
-      dispatch(setDogsNext(dirtyDogs.pagination._links.next.href))
-      dispatch(fetchDogsSuccess(cleanedAndAddedDogs))
+      dispatch(setSearchTotalPages(dirtyDogs.pagination.total_pages))
+      if (dirtyDogs.pagination._links.next) dispatch(setDogsNext(dirtyDogs.pagination._links.next.href))
+      nextSearch ? dispatch(fetchAdditionalDogsSuccess(cleanedAndAddedDogs))
+        : dispatch(fetchDogsSuccess(cleanedAndAddedDogs))
       dispatch(setLoading(false))
     } catch (error) {
       dispatch(hasErrored(error.message))
