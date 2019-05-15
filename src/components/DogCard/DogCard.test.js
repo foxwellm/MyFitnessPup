3a51c0b522add91fd259808f15bb96a1e8041121
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-14 20:33:25 -0600

    Update DogCard tests

--- src/components/DogCard/DogCard.test.js
+++ src/components/DogCard/DogCard.test.js
@@ -4,8 +4,46 @@ import DogCard from './DogCard'
 
 describe('DogCard', () => {
   let wrapper
+  let mockPhoto
+  let mockName
+  let mockAge
+  let mockDistance
+
+  beforeEach(() => {
+    mockPhoto = 'photoUrl'
+    mockName= 'Butch'
+    mockAge= 'Young'
+    mockDistance = '17.0 mi'
+    wrapper = shallow(<DogCard 
+        photo={mockPhoto}
+        name={mockName}
+        age={mockAge}
+        distance={mockDistance}
+      />)
+  })
   it('should have match the correct snapshot', () => {
-    wrapper = shallow(<DogCard />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should have match the correct snapshot when the dog has multiple names', () => {
+    mockName = 'Butch Casidy'
+    wrapper = shallow(<DogCard
+      photo={mockPhoto}
+      name={mockName}
+      age={mockAge}
+      distance={mockDistance}
+    />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should have match the correct snapshot when the dog has no name', () => {
+    mockName = ''
+    wrapper = shallow(<DogCard
+      photo={mockPhoto}
+      name={mockName}
+      age={mockAge}
+      distance={mockDistance}
+    />)
     expect(wrapper).toMatchSnapshot()
   })
 })
