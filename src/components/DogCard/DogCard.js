94789d487d9baffc9ef497e3cbfc3703d57b61f3
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-21 10:50:17 -0600

    Add heart transition to DogCard

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -4,18 +4,21 @@ let shortID = require('short-id');
 
 export const DogCard = (props) => {
 
-    const { photo, name, age, distance, id } = props
-    const dogName = !name || !name.match(' ') ? name : name.split(' ')[0]
-    return (
-      <Link className='DogCard-container' to={`/dog/${id}`} key={shortID.generate()}>
-        <div className='dog-name'>{dogName}</div>
-        <div className='dog-age'>Age: {age}</div>
-        <div className='dog-distance'>Location: {distance} away</div>
-        <div className='search-img-container'>
-          <img className='dog-img' alt='dog from shelter' src={photo}></img>
+  const { photo, name, distance, id } = props
+  const dogName = !name || !name.match(' ') ? name : name.split(' ')[0]
+  return (
+    <Link className='DogCard-container' to={`/dog/${id}`} key={shortID.generate()}>
+      <div className='info-container'>
+        <div className='info-inner-container'>
+          <div className='dog-name'>{dogName}</div>
+          <div className='search-img-container'>
+            <img className='dog-img' alt='dog from shelter' src={photo}></img>
+          </div>
+          <div className='dog-distance'>{distance} away</div>
         </div>
-      </Link>
-    )
+      </div>
+    </Link>
+  )
 }
 
 export default DogCard
