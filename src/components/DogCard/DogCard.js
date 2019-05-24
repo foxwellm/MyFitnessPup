4fab37817d9dec38d944111798fe3db672fa5000
Matthew Foxwell <foxwellmw@gmail.com>
2019-05-24 14:36:45 -0600

    Add image for no picture available

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import nopic from '../../assets/images/nopic.png'
 let shortID = require('short-id');
 
 export const DogCard = (props) => {
@@ -12,7 +13,7 @@ export const DogCard = (props) => {
         <div className='info-inner-container'>
           <div className='dog-name'>{dogName}</div>
           <div className='search-img-container'>
-            <img className='dog-img' alt='dog from shelter' src={photo}></img>
+            <img className='dog-img' alt='dog from shelter' src={photo === null ? nopic : photo}></img>
           </div>
           <div className='dog-distance'>{distance} away</div>
         </div>
